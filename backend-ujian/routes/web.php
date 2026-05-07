30cb99b2031b7510195b1dd37db297691ad127a6
Wibii <edukasikulaih7@gmail.com>
2026-05-07 11:27:02 +0700

    Downgrade Laravel framework from v13 to v12 for stability

--- backend-ujian/routes/web.php
+++ backend-ujian/routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Admin\ScheduleController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 /// Group untuk Admin
